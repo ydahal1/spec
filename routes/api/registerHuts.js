49ef38abe2f1b02e95527825c7e0f8f5d6448827
Yadhap Dahal <yadhapdahal111@gmail.com>
2019-08-30 18:56:46 -0400

    added comments for testing

--- routes/api/registerHuts.js
+++ routes/api/registerHuts.js
@@ -1,73 +1,73 @@
 const express = require('express');
-const { check, validationResult} = require('express-validator');
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
 const Huts = require('../../models/Huts');
 
 //@route POST api/huts
 //@des    register Huts route
 //@access  Public
-router.post('/', 
-[
-    check('familyLastName', 'Family Last Name is required').not().isEmpty(),
+router.post('/',
+    [
+        check('familyLastName', 'Family Last Name is required').not().isEmpty(),
 
-    check('camp', 'Please select Camp').not().isEmpty(),
-    check('sector', 'Please select Sector').not().isEmpty(),
-    check('unit', 'Please select Unit').not().isEmpty(),
-    check('hutNumber1', 'Hut number is incorrect').not().isEmpty(),
+        check('camp', 'Please select Camp').not().isEmpty(),
+        check('sector', 'Please select Sector').not().isEmpty(),
+        check('unit', 'Please select Unit').not().isEmpty(),
+        check('hutNumber1', 'Hut number is incorrect').not().isEmpty(),
 
-    check('district', 'incorrect district entered').not().isEmpty(),
-    check('village', 'incorrect village entered').not().isEmpty(),
+        check('district', 'incorrect district entered').not().isEmpty(),
+        check('village', 'incorrect village entered').not().isEmpty(),
 
-    check('country', 'Please select country').not().isEmpty(),
-    check('state', 'state is incomplete').not().isEmpty(),
-    check('city', 'Camp address is incomplete').not().isEmpty(),
-    check('phone', 'Invalid phone number').isMobilePhone(),
+        check('country', 'Please select country').not().isEmpty(),
+        check('state', 'state is incomplete').not().isEmpty(),
+        check('city', 'Camp address is incomplete').not().isEmpty(),
+        check('phone', 'Invalid phone number').isMobilePhone(),
 
-    check('members', 'Atleast one member should be listed').not().isEmpty(),
-    
-],
-async(req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()})
-    }
-    //Getting items from request to check if the hut is already registered
-    
-    const {camp} = req.body;
-    const{sector} = req.body;
-    const{unit}=req.body;
-    const{hutNumber1} = req.body;
-   
+        check('members', 'Atleast one member should be listed').not().isEmpty(),
 
-    try{
-        //Tests:
-        // console.log(familyLastName);
-        console.log(typeof registeredOn);
-        console.log(typeof familyStory)
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        //Getting items from request to check if the hut is already registered
 
-        //Check if the hut is already registered
-        let hut = await Huts.findOne({
-            $and :[{camp,sector, unit, hutNumber1}]
-            
-                        });
+        const { camp } = req.body;
+        const { sector } = req.body;
+        const { unit } = req.body;
+        const { hutNumber1 } = req.body;
+
+
+        try {
+            //Tests:
+            // console.log(familyLastName);
+            console.log(typeof registeredOn);
+            console.log(typeof familyStory)
+
+            //Check if the hut is already registered
+            let hut = await Huts.findOne({
+                $and: [{ camp, sector, unit, hutNumber1 }]
+
+            });
             // if the hut is found to be registered
-            if(hut){
-                res.status(400).json({errors : [{msg: 'This hut is already registered'}]})
-            }else{
+            if (hut) {
+                res.status(400).json({ errors: [{ msg: 'This hut is already registered' }] })
+            } else {
 
-                //I AM HERE ....
+                //I AM HERE ......
                 hut.save();
                 res.json(req.body);
 
 
             }
 
-    }catch(err){
-        console.error(err.message);
-        res.status(500).send('server error')
-    }
-    
-   })
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('server error')
+        }
+
+    })
 
 module.exports = router;
 
