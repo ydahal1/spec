d0f1c4ffc28f86a0b5af12b07edda18d53f72fdd
ydahal1 <yadhapdahal@yadhaps-Mini.attlocal.net>
2019-08-31 11:18:06 -0400

    started with react but need to delete client and re install react

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+import './App.css';
+
+const  App = () =>  
+    <Fragment>
+      <h1> Hello Yadhap</h1>
+      
+    </Fragment>
+
+
+
+export default App;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const NavBar = () => {
+    return ( 
+        <div>
+            <nav>
+                <p>Hello from the Navbar</p>
+            </nav>
+        </div>
+     );
+}
+ 
+export default NavBar;
