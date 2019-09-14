d70cd73bb2c07bbd1a61944e618854d4d0f6bd39
Yadhap Dahal <yadhapdahal111@gmail.com>
2019-09-14 10:49:13 -0400

    commiting to pull from origin master

--- client/src/components/loginWithButtons.js
+++ client/src/components/loginWithButtons.js
@@ -1,7 +1,37 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import FacebookLogin from 'react-facebook-login';
+import FacebookConfig from '../../../config';
+
 class LoginWithSocial extends Component {
-    state = {}
+    state = {
+        isLoggedIn: false,
+        usersID: '',
+        name: '',
+        email: '',
+        picture: ''
+    }
+    responseFacebook = response => {
+        console.log(response);
+    }
+    componentClicked = () => {
+        console.log('clicked here yadahp');
+    }
     render() {
+        let fbContent;
+        if (this.state.isLoggedIn) {
+            fbContent = null;
+
+        } else {
+            fbContent = (
+                <FacebookLogin
+                    appId={FacebookConfig.appId}
+                    autoLoad={true}
+                    fields="name,email,picture"
+                    onClick={this.componentClicked}
+                    callback={this.responseFacebook} />
+            )
+        }
         const wholeComponent = {
             border: "1px solid black",
             padding: "20px",
@@ -9,12 +39,14 @@ class LoginWithSocial extends Component {
         }
         return (
             <div style={wholeComponent}>
+                {fbContent}
                 <div>
                     <button>Register a hut</button>
                 </div>
                 <div>
                     <button>Sign in with google</button>
-                    <a href={'auth/facebook'}> sig in with facebook</a>
+                    <a href={'/login/facebook'}>Facebook</a>
+
                 </div>
             </div >
         );
--- routes/api/facebookAuth.js
+++ routes/api/facebookAuth.js
@@ -4,38 +4,37 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 const fbAuth = require("../../config/fbAuth.js")
 const router = express.Router();
 
+passport.use(new FacebookStrategy({
+  clientID: fbAuth.facebookAuth.clientID,
+  clientSecret: fbAuth.facebookAuth.clientSecret,
+  callbackURL: fbAuth.facebookAuth.callbackURL
+},
+  async (accessToken, refreshToken, profile, done) => {
+    try {
+      console.log('profile', profile);
+      console.log('accessToken', accessToken);
+      console.log('refreshToken', refreshToken);
+
+    } catch (err) {
+      done(err, false, err.message)
+    }
+  }
+));
+
+
+// router.get('/', passport.authenticate('facebook'));
 
 router.get('/', (req, res) => {
-  res.send("Hello facebook")
-});
+  res.send("Hello my froiend from dacebook")
+})
 
-
-
-// router.get('/auth/facebook', passport.authenticate('facebook'));
+// app.get('/login/facebook',
+//   passport.authenticate('facebook'));
 
 // router.get('/auth/facebook/callback',
 //   passport.authenticate('facebook', {
 //     successRedirect: '/dashboard',
 //     failureRedirect: '/login'
 //   }));
-
-
-
-// passport.use(new FacebookStrategy({
-//   clientID: fbAuth.facebookAuth.clientID,
-//   clientSecret: fbAuth.facebookAuth.clientSecret,
-//   callbackURL: fbAuth.facebookAuth.callbackURL
-// },
-//   async (accessToken, refreshToken, profile, done) => {
-//     try {
-//       console.log('profile', profile);
-//       console.log('accessToken', accessToken);
-//       console.log('refreshToken', refreshToken);
-
-//     } catch (err) {
-//       done(err, false, err.message)
-//     }
-//   }
-// ));
 
 module.exports = router;
